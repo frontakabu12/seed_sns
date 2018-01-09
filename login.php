05b068d5090145aebf3051dd012d213d72bbc00e
Kana Ando <kanaa@KananoMacBook-Pro.local>
2018-01-09 15:49:00 +0800

    自動ログイン完成

--- login.php
+++ login.php
@@ -1,3 +1,79 @@
+<?php
+session_start();
+
+// クッキー情報が存在していたら(自動ログイン)
+// $_POSTにログイン情報を保存する
+if(isset($_COOKIE["email"]) && !empty($_COOKIE["email"])){
+  $_POST["email"] = $_COOKIE["email"];
+  $_POST["password"] = $_COOKIE["password"];
+  $_POST["save"] = "on";
+}
+
+//DBに接続
+require('dbconnect.php');
+
+
+// POST送信されていたら
+if(isset($_POST) && !empty($_POST)){
+  // 認証処理
+  try{
+    // メンバーズテーブルでテーブルの中からメールアドレスとパスワードが入力されたものと合致するデータを取得
+    $sql = "SELECT * FROM `members` WHERE `email`=? AND `password`=? ";
+
+    // sql文実行
+    // パスワードは、入力されたものを暗号化した上で使用する
+    $data = array($_POST["email"], sha1($_POST["password"]));
+    $stmt = $dbh->prepare($sql);
+    $stmt->execute($data);
+
+    // 一行取得
+    $member = $stmt->fetch(PDO::FETCH_ASSOC);
+
+    // echo '<pre>';
+    // var_dump($member);
+    // echo '</pre>';
+
+
+    if($member == false){
+      // 認証失敗
+      $error["login"] = "failed";
+
+    }else{
+      // 認証成功
+      // 1.セッション変数に会員のIDを保存
+      $_SESSION["id"] = $member["member_id"];
+
+      // 2.ログインした時間をセッション変数に保存
+      $_SESSION["time"] = time();
+
+      // 3.自動ログインの処理
+      if($_POST["save"] == "on"){
+        // cookieにログイン情報を記録
+        // setcookie(保存したい名前,保存したい値, 保存したい期間:秒数)
+        setcookie('email', $_POST["email"], time()+60*60*24*14);
+        setcookie('password', $_POST["password"], time()+60*60*24*14);
+      }
+
+
+
+      // 4.ログイン後の画面に移動
+      header("Location: index.php");
+      exit();
+    }
+  }catch(Exception $e){
+
+  }
+
+}
+
+
+
+
+?>
+
+
+
+
 <!DOCTYPE html>
 <html lang="ja">
   <head>
@@ -57,6 +133,19 @@
               <input type="password" name="password" class="form-control" placeholder="">
             </div>
           </div>
+
+          <!-- 自動ログイン -->
+          <div class="form-group">
+            <label class="col-sm-4 control-label">自動ログイン</label>
+            <div class="col-sm-8">
+              <!-- name="save" →POST送信の項目 -->
+              <input type="checkbox" name="save">オンにする
+            </div>
+          </div>
+
+          <?php if((isset($error["login"])) && ($error['login'] == 'failed')){ ?>
+              <p class="error">* emailかパスワードが間違っています。</p>
+              <?php } ?>
           <input type="submit" class="btn btn-default" value="ログイン">
         </form>
       </div>
